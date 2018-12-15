8b30f608b1396aa23a2c61e836b7bc3097c5a0a5
Tormod Mathiesen <tormathi@gmail.com>
2018-12-15 13:49:06 +0100

    day 1 part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,7 +1,10 @@
 module Day1 where
 
-import           Util                           ( split )
+import           Util                           ( split
+                                                , dup
+                                                )
 import qualified Data.Text                     as T
+import           Data.Maybe                     ( fromMaybe )
 
 -- Part 1
 
@@ -14,18 +17,33 @@ readInts :: String -> Char -> [Int]
 readInts "" _  = []
 readInts s  sp = map (readInt . T.unpack . T.strip . T.pack) (split sp s)
 
-frequency :: String -> Int
-frequency s = sum $ readInts s ','
-
-filename = "resources/day1-1.txt"
+filename = "resources/day1.txt"
 
 readIntFile :: String -> IO [Int]
 readIntFile fn = do
     str <- readFile fn
     return (readInts str '\n')
 
-frequencyFile :: IO Int
-frequencyFile = do
+frequency :: IO Int
+frequency = do
     ints <- readIntFile filename
     return $ sum ints
 
+-- Part 2
+
+getSums :: [Int] -> [Int]
+getSums ns = 0 : accsum 0 ns
+  where
+    accsum i []       = []
+    accsum i (n : ns) = i + n : accsum (i + n) ns
+
+findDup :: [Int] -> Int
+findDup ns = fromMaybe (error "file doesn't have a duplicate!") (dup ns)
+
+repeatFreqUntilDup :: [Int] -> Int
+repeatFreqUntilDup ns = fromMaybe (repeatFreqUntilDup (ns ++ ns)) (dup (getSums ns))
+
+firstDuplicateFrequency :: IO Int
+firstDuplicateFrequency = do
+    ints <- readIntFile filename
+    return $ repeatFreqUntilDup ints
--- src/Util.hs
+++ src/Util.hs
@@ -1,5 +1,18 @@
-module Util (split) where
+module Util
+    ( split
+    , dup
+    )
+where
+
+import qualified Data.Set                      as S
 
 split :: Char -> String -> [String]
 split _ "" = []
 split c s  = takeWhile (/= c) s : split c (drop 1 (dropWhile (/= c) s))
+
+dup' :: (Ord a) => [a] -> S.Set a -> Maybe a
+dup' []       _   = Nothing
+dup' (x : xs) set = if S.member x set then Just x else dup' xs (S.insert x set)
+
+dup :: (Ord a) => [a] -> Maybe a
+dup xs = dup' xs S.empty
